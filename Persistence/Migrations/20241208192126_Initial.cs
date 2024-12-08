95065c853309c5a412951b58968a9b28cae7bd1a
Jonaunk <paez.jonathan@outlook.com>
2024-12-08 16:24:55 -0300

    migrations

---
+++ Persistence/Migrations/20241208192126_Initial.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Provincia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UsuarioAlta = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioModificacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaModificacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UsuarioBaja = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provincia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ciudad",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProvinciaId = table.Column<int>(type: "int", nullable: true),
+                    UsuarioAlta = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioModificacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaModificacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UsuarioBaja = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudad", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ciudad_Provincia_ProvinciaId",
+                        column: x => x.ProvinciaId,
+                        principalTable: "Provincia",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contactos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CiudadId = table.Column<int>(type: "int", nullable: true),
+                    ImagenPerfil = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UsuarioAlta = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioModificacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaModificacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UsuarioBaja = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contactos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contactos_Ciudad_CiudadId",
+                        column: x => x.CiudadId,
+                        principalTable: "Ciudad",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ciudad_ProvinciaId",
+                table: "Ciudad",
+                column: "ProvinciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contactos_CiudadId",
+                table: "Contactos",
+                column: "CiudadId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contactos");
+
+            migrationBuilder.DropTable(
+                name: "Ciudad");
+
+            migrationBuilder.DropTable(
+                name: "Provincia");
+        }
+    }
+}
